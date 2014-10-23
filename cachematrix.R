b9e875ef74a5aa9f386bcf5beb86990dd16afbbe
chaiyuan04 <chaiyuan04@gmail.com>
2014-10-23 18:29:31 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## function "makeCacheMatrix" creates a special "matrix" object that can cache its inverse 
+## functions "cacheSolve" computes the inverse of the special "matri"
 
-## Write a short comment describing this function
+# makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(invM) inv <<- invM
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+# cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+# If the inverse has already been calculated (and the matrix has not changed), 
+# then the cachesolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, y=x$get(), ...) {  
+  #x is the special matrix created by makeCacheMatrix.
+  #y is the original matrix  that was used to make x; allow user to check if matrix changed; default as x$get()
+  idcheck <- identical(x$get(), y)  #Compare to see if the matrix cached has changed compared to original matrix
+  inv <- x$getinv()
+  
+  if(idcheck & !is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  data <- x$get()
+  inv <- solve(data)
+  x$setinv(inv)
+  inv
 }
